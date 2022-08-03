0bb4f74c17c40a555e4d68eb13569b996d78b52c
HeyImRobin <106459877+HeyImRobin@users.noreply.github.com>
2022-08-03 00:39:27 -0400

    Revert "Dev"

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,44 +1,14 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
 ?>
-<div class="container-fluid">
-    <h1>Home</h1>
-
-
-    <h3>Highscores</h3>
-
-
-    <form action="http://localhost:3000/Project/game.php">
-         <button type="submit">Click me to play</button>
-      </form>
-
-
-
-    <?php
-
-
-
-
-
-    ?>
-
-    <?php
-    //this is day which is the default
-    require(__DIR__ . "/../../partials/scores_table.php");
-    ?>
-    <?php
-    $duration = "week";
-    require(__DIR__ . "/../../partials/scores_table.php");
-    ?>
-    <?php
-    $duration = "month";
-    require(__DIR__ . "/../../partials/scores_table.php");
-    ?>
-    <?php
-    $duration = "lifetime";
-    require(__DIR__ . "/../../partials/scores_table.php");
-    ?>
-</div>
+<h1>Home</h1>
 <?php
-// require(__DIR__ . "/../../partials/footer.php");
+
+if (is_logged_in(true)) {
+    //comment this out if you don't want to see the session variables
+    error_log("Session data: " . var_export($_SESSION, true));
+}
+?>
+<?php
+require(__DIR__ . "/../../partials/flash.php");
 ?>
